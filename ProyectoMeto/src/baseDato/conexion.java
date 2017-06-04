35615380ac02d32cadcbd83520febe7baa6248e7
unknown <c.fuentescorts@gmail.com>
2017-06-04 18:55:31 -0400

    agregacion de adm

--- ProyectoMeto/src/baseDato/conexion.java
+++ ProyectoMeto/src/baseDato/conexion.java
@@ -5,10 +5,34 @@
  */
 package baseDato;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author carlos
+ * @author Pablo
  */
-public class conexion {
+public class Conexion {
+    public  static Statement stmt;
+    public static Connection con;
     
+    public static Connection getConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+        }catch(java.lang.ClassNotFoundException err){
+            JOptionPane.showMessageDialog(null,"ERROR: No se encuentra el driver de MYSQL");
+        }
+        try{
+            //con=DriverManager.getConnection("jdbc:mysql://localhost/dulzura","root","felipe0419");
+            con=DriverManager.getConnection("jdbc:mysql://localhost/dulzura","root","Dragon1234"); //CARLOS
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(null,"ERROR: No se pudo conectar a base de datos");
+            System.exit(0);
+        }
+        return con;
+    }
 }
